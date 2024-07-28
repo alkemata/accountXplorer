e6a004228b585c14285514a8493841053684730b
alkemata <42153661+alkemata@users.noreply.github.com>
2024-07-28 16:16:25 +0200

    debug

--- app/overview.py
+++ app/overview.py
@@ -8,6 +8,7 @@ def create_dash_app(server):
     app = dash.Dash(__name__, server=server, url_base_pathname='/app1/')
     df = pd.read_csv('./ressources/dataliste.csv',sep=';')
     df['Betrag'] = pd.to_numeric(df['Betrag'], errors='coerce')
+    print(df['Betrag'])
 
     style_data_conditional = [
         {
