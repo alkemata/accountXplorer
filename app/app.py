7862eaa6f49135f555dc06cbd4bf5f1d1d1f8595
alkemata <42153661+alkemata@users.noreply.github.com>
2024-07-28 10:33:08 +0200

    correct

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 from werkzeug.security import generate_password_hash, check_password_hash
-from models import db, User, init_db
+from models import db, User
 import config
 
 app = Flask(__name__)
