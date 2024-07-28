b07799b6f75dd180fa23a1218650831fddf09902
alkemata <42153661+alkemata@users.noreply.github.com>
2024-07-28 15:54:57 +0200

    debug

--- app/overview.py
+++ app/overview.py
@@ -1,14 +1,13 @@
 import dash
 import pandas as pd
 from dash import html
-import dash_core_components as dcc
 from dash import dcc, html, dash_table
 from dash.dependencies import Input, Output, State
 
 def create_dash_app(server):
     app = dash.Dash(__name__, server=server, url_base_pathname='/app1/')
     df = pd.read_csv('./ressources/dataliste.csv',sep=';')
-    
+    print(df)
     style_data_conditional = [
         {
             'if': {'filter_query': '{Betrag} > 0', 'column_id': 'betrag'},
@@ -22,7 +21,7 @@ def create_dash_app(server):
     dash_table.DataTable(
         id='table',
         columns=[{'name': col, 'id': col} for col in df.columns],
-        data=[df.to_dict('records'), [{"name": i, "id": i} for i in df.columns]],
+        data=[df.to_dict('records')],
         style_data_conditional=style_data_conditional,
         style_table={'overflowX': 'auto'}
     )
