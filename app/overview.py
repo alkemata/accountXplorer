430e16cdde4342467b678c2bff632e10e1915726
alkemata <42153661+alkemata@users.noreply.github.com>
2024-07-28 15:30:11 +0200

    correct dependencies

--- app/overview.py
+++ app/overview.py
@@ -1,6 +1,8 @@
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
+from dash import dcc, html, dash_table
+from dash.dependencies import Input, Output, State
 
 def create_dash_app(server):
     app = dash.Dash(__name__, server=server, url_base_pathname='/app1/')
