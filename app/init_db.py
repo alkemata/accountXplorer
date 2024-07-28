673aa1f4b0e19ea49be35e8055a7b0f50c838ac9
alkemata <42153661+alkemata@users.noreply.github.com>
2024-07-28 11:00:08 +0200

    error catching

--- app/init_db.py
+++ app/init_db.py
@@ -4,15 +4,19 @@ from models import User
 
 def init_db():
     with app.app_context():
-        db.create_all()  # Create tables
-        # Check if the initial user exists
-        if not User.query.filter_by(username=app.config['INITIAL_USERNAME']).first():
-            # Add initial user
-            hashed_password = generate_password_hash(app.config['INITIAL_PASSWORD'], method='sha256')
-            new_user = User(username=app.config['INITIAL_USERNAME'], password=hashed_password)
-            db.session.add(new_user)
-            db.session.commit()
-            print("Initialized the database and added the initial user.")
+        try: 
+            db.create_all()  # Create tables
+            # Check if the initial user exists
+            if not User.query.filter_by(username=app.config['INITIAL_USERNAME']).first():
+                # Add initial user
+                hashed_password = generate_password_hash(app.config['INITIAL_PASSWORD'], method='sha256')
+                new_user = User(username=app.config['INITIAL_USERNAME'], password=hashed_password)
+                db.session.add(new_user)
+                db.session.commit()
+                print("Initialized the database and added the initial user.")
+        except Exception as e:
+            logger.error("Error initializing the database: %s", e)
+            raise
 
 if __name__ == '__main__':
     init_db()
