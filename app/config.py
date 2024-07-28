1b31cd2b2f603723b960ca89f774dede11925a9f
alkemata <42153661+alkemata@users.noreply.github.com>
2024-07-28 11:06:30 +0200

    correct

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'supersecretkey'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///users.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////app/users.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     INITIAL_USERNAME = os.environ.get('INITIAL_USERNAME')
     INITIAL_PASSWORD = os.environ.get('INITIAL_PASSWORD')
