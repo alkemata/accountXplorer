1f1d3eb851a703bc06cffc214bce2837133b9fe9
alkemata <42153661+alkemata@users.noreply.github.com>
2024-07-28 15:45:10 +0200

    csv reading

--- app/overview.py
+++ app/overview.py
@@ -1,13 +1,13 @@
 import dash
 import pandas as pd
-import dash_html_components as html
+from dash import html
 import dash_core_components as dcc
 from dash import dcc, html, dash_table
 from dash.dependencies import Input, Output, State
 
 def create_dash_app(server):
     app = dash.Dash(__name__, server=server, url_base_pathname='/app1/')
-    df = pd.read_csv('./ressources/dataliste.csv')
+    df = pd.read_csv('./ressources/dataliste.csv',sep=';')
 
     app.layout = html.Div([
 
