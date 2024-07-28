2fde83c7ecd4f8f399fd2812890554988321ce7a
alkemata <42153661+alkemata@users.noreply.github.com>
2024-07-28 16:15:06 +0200

    numerical betrag

--- app/overview.py
+++ app/overview.py
@@ -7,6 +7,7 @@ from dash.dependencies import Input, Output, State
 def create_dash_app(server):
     app = dash.Dash(__name__, server=server, url_base_pathname='/app1/')
     df = pd.read_csv('./ressources/dataliste.csv',sep=';')
+    df['Betrag'] = pd.to_numeric(df['Betrag'], errors='coerce')
 
     style_data_conditional = [
         {
