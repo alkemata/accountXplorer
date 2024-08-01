3e2b03b197c98dabd5d65beec1c196b91c938fe7
alkemata <42153661+alkemata@users.noreply.github.com>
2024-08-01 16:25:03 +0200

    debug

--- app/edit.py
+++ app/edit.py
@@ -110,7 +110,7 @@ def create_dash_app(server):
             filtered_df = df[(df['Kategorie'] == category) & (df['Month'] == month)]
             filtered_df=filtered_df.drop(columns=['Month'])
             logger.info(filtered_df.to_dict('records'))
-            return filtered_df.to_dict('records')
+            return filtered_df
         return 'nothing'
 
     @app.callback(
