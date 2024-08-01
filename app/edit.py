9026c38a08c3bfeb62ba4fbd2bf942ce60d110ad
alkemata <42153661+alkemata@users.noreply.github.com>
2024-08-01 16:58:54 +0200

    debug

--- app/edit.py
+++ app/edit.py
@@ -112,7 +112,7 @@ def create_dash_app(server):
             filtered_df = df[(df['Kategorie'] == category) & (df['Month'] == month)]
             filtered_df=filtered_df.drop(columns=['Month'])
             filtered_df['Buchungsdatum']=filtered_df['Buchungsdatum'].astype(str)
-            return filtered_df.to_dict('records')
+            return json.dumps(filtered_df.to_dict('records')),filtered_df.to_dict('records')
         return 'nothing'
 
     @app.callback(
