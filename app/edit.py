a7b42842f8ff566eb4838591dfb5fcdef59b70ad
alkemata <42153661+alkemata@users.noreply.github.com>
2024-08-01 16:59:28 +0200

    debug

--- app/edit.py
+++ app/edit.py
@@ -113,7 +113,7 @@ def create_dash_app(server):
             filtered_df=filtered_df.drop(columns=['Month'])
             filtered_df['Buchungsdatum']=filtered_df['Buchungsdatum'].astype(str)
             return json.dumps(filtered_df.to_dict('records')),filtered_df.to_dict('records')
-        return 'nothing'
+        return 'nothing','nothing'
 
     @app.callback(
         [Output('pivot-table', 'data'),
