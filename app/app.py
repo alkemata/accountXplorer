f9204eb8592bd7686643c0aa375c4aa85c4f6883
alkemata <42153661+alkemata@users.noreply.github.com>
2024-08-09 07:22:58 +0200

    correct

--- app/app.py
+++ app/app.py
@@ -11,6 +11,7 @@ import dash
 #import dash_html_components as html
 #import dash_core_components as dcc
 
+server = Flask(__name__)
 app=dash.Dash(__name__, server=server, suppress_callback_exceptions=True)
 #app = Flask(__name__)
 app.config.from_object(config.Config)
