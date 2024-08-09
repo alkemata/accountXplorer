aa0b8b64f36825d5d964bd88a283d2c3c0910933
alkemata <42153661+alkemata@users.noreply.github.com>
2024-08-10 00:29:45 +0200

    correct

--- app/appmanager/auth.py
+++ app/appmanager/auth.py
@@ -13,7 +13,7 @@ def login():
         password = request.form.get('password')
         user = User.query.filter_by(username=username).first()
 
-        if user and check_password_hash(user.password, password):
+        if user and check_password_hash(user.password_hash, password):
             login_user(user)
             return redirect(url_for('main.index'))
 
