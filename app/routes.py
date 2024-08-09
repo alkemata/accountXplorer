f8ec923949fc99489365db3563263e735fac9a76
alkemata <42153661+alkemata@users.noreply.github.com>
2024-08-09 17:30:12 +0200

    correct

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request
 from flask_login import login_user, current_user, logout_user, login_required
-import app, db
+from app import app, db
 from models import User, App
 from sync_apps_directory import sync_apps_directory
 
