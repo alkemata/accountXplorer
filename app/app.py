09f646617f79f6c4bdf3b33abb1d1c83f4832999
alkemata <42153661+alkemata@users.noreply.github.com>
2024-08-09 20:05:14 +0200

    correct

--- app/app.py
+++ app/app.py
@@ -5,7 +5,6 @@ from flask_bcrypt import Bcrypt
 from config import Config
 from flask import render_template, url_for, flash, redirect, request
 from flask_login import login_user, current_user, logout_user, login_required
-from app import app, db
 from models import User, App
 from init_db import sync_apps_directory
 
