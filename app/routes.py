6ccece9fc97d76284dec8085a2d7318865c2dfb3
alkemata <42153661+alkemata@users.noreply.github.com>
2024-08-09 17:31:54 +0200

    correct

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, url_for, flash, redirect, request
 from flask_login import login_user, current_user, logout_user, login_required
 from app import app, db
 from models import User, App
-from sync_apps_directory import sync_apps_directory
+from init_db import sync_apps_directory
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
