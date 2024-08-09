2043fd441503f9e8eecd4eb0ac5d54605d9a8a2a
alkemata <42153661+alkemata@users.noreply.github.com>
2024-08-09 17:28:20 +0200

    correct

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
-from your_project.config import Config
+from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
