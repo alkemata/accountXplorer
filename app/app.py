0e0774c6bd97ff715da88ce62043866a6be527ed
alkemata <42153661+alkemata@users.noreply.github.com>
2024-08-09 20:04:34 +0200

    correct

--- app/app.py
+++ app/app.py
@@ -3,6 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from config import Config
+from flask import render_template, url_for, flash, redirect, request
+from flask_login import login_user, current_user, logout_user, login_required
+from app import app, db
+from models import User, App
+from init_db import sync_apps_directory
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -14,7 +20,37 @@ bcrypt = Bcrypt(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
-import routes
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.check_password(password):
+            login_user(user)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('./templates/login.html')
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/home')
+@login_required
+def home():
+    # Sync the apps directory with the database before rendering
+    sync_apps_directory()
+    
+    # Only show the apps that the user is authorized for and that still exist in the directory
+    authorized_apps = [app for app in current_user.authorized_apps if os.path.exists(app.path)]
+    
+    return render_template('home.html', apps=authorized_apps)
 
 if __name__ == '__main__':
     app.run(debug=True)
