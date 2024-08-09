3d3ccff3c9c56f3d81293e16706eca0dd57b5cdf
alkemata <42153661+alkemata@users.noreply.github.com>
2024-08-10 00:26:45 +0200

    correct

--- app/appmanager/auth.py
+++ app/appmanager/auth.py
@@ -9,15 +9,15 @@ auth_blueprint = Blueprint('auth', __name__)
 @auth_blueprint.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        email = request.form.get('email')
+        email = request.form.get('username')
         password = request.form.get('password')
-        user = User.query.filter_by(email=email).first()
+        user = User.query.filter_by(username=username).first()
 
         if user and check_password_hash(user.password, password):
             login_user(user)
             return redirect(url_for('main.index'))
 
-        flash('Login Unsuccessful. Please check email and password.', 'danger')
+        flash('Login Unsuccessful. Please check username and password.', 'danger')
 
     return render_template('login.html')
 
