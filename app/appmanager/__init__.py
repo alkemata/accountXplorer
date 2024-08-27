e44bae5bf30aa299a6db232f96a37cae9d91daca
alkemata <42153661+alkemata@users.noreply.github.com>
2024-08-27 12:28:05 +0200

    correct

--- app/appmanager/__init__.py
+++ app/appmanager/__init__.py
@@ -4,6 +4,7 @@ from flask_login import LoginManager
 from .models import db, User
 from .routes import main_blueprint
 from .auth import auth_blueprint  # Import the auth blueprint
+from FinanceX import FinanceX
 
 def create_app():
     app = Flask(__name__)
@@ -28,5 +29,6 @@ def create_app():
     # Register blueprints
     app.register_blueprint(main_blueprint)
     app.register_blueprint(auth_blueprint)
+    app.register_blueprint(FinanceX)
 
     return app
