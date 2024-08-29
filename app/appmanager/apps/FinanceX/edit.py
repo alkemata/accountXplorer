57cbf8e49278aab43005ddc8e7e44752d8bba5d9
alkemata <42153661+alkemata@users.noreply.github.com>
2024-08-29 13:35:04 +0200

    correct

--- app/appmanager/apps/FinanceX/edit.py
+++ app/appmanager/apps/FinanceX/edit.py
@@ -16,7 +16,7 @@ logger = logging.getLogger()
 app = dash.Dash(__name__,  url_base_pathname='/edit/', external_stylesheets=[dbc.themes.BOOTSTRAP])
 logger.info('logger 2 activated')
 app.layout=html.Div([dcc.Store(id='shared-dataframe'), edit_ui.layout_files()])
- 
+app.run_server(debug=True)
 
     #recurrent_expenses = df.groupby('Verwendungszweck').filter(lambda x: len(x) > 1).drop(columns=['Month', 'IBAN', 'Umbuchung', 'Buchungstext'])
     #recurrent_expenses = recurrent_expenses.sort_values(by='Verwendungszweck')
