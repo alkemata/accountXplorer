0eb1ac66221424a2782969f304e2917b2cae5d5d
alkemata <42153661+alkemata@users.noreply.github.com>
2024-08-29 18:02:42 +0200

    correct

--- app/FinanceX/functions.py
+++ app/FinanceX/functions.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import os
-ressources_dir = '/app/FinanceX/resources/'
+ressources_dir = './FinanceX/resources/'
 
 def load_categories(file_path):
     with open(file_path, 'r') as file:
