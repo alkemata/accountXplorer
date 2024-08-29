4e97e43189df8bb3395910f0fd7eaba373c30574
alkemata <42153661+alkemata@users.noreply.github.com>
2024-08-29 13:55:58 +0200

    correct

--- app/appmanager/apps/FinanceX/financeX.py
+++ app/appmanager/apps/FinanceX/financeX.py
@@ -4,10 +4,8 @@ from .edit import create_dash_app
 from flask import current_app
 
 # Define the blueprint
-with current_app.app_context():
-    FinanceX = Blueprint('FinanceX', __name__, template_folder='templates')
-    print(current_app)
-    appedit=create_dash_app(current_app)
+FinanceX = Blueprint('FinanceX', __name__, template_folder='templates')
+appedit=create_dash_app(current_app)
 
 # Route for the first page
 @FinanceX.route('/financeX/overview')
