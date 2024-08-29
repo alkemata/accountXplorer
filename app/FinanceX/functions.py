fe8e0013fec083c8a5839f0b70a30ca139cfe1f6
alkemata <42153661+alkemata@users.noreply.github.com>
2024-08-29 23:15:55 +0200

    correct

--- app/FinanceX/functions.py
+++ app/FinanceX/functions.py
@@ -64,7 +64,7 @@ def detect_transfers(row):
 
 def merge_new_data(file1, file2):
     try:
-        df_existing = pd.read_csv(os.path.join(ressources_dir,file2),separator=';')
+        df_existing = pd.read_csv(os.path.join(ressources_dir,file2),sep=';')
     except Exception as e:
         print('File note found')
         return {'code': 0, 'msg':  str(e) + ' - '+os.path.join(ressources_dir,file2)}
