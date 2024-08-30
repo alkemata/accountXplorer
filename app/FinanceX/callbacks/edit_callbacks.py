c34b66f1646369d91e5b93d0e0b92c3eb4db6088
alkemata <42153661+alkemata@users.noreply.github.com>
2024-08-30 22:46:43 +0200

    correct

--- app/FinanceX/callbacks/edit_callbacks.py
+++ app/FinanceX/callbacks/edit_callbacks.py
@@ -22,6 +22,7 @@ def display_details(active_cell,pivot, dataframe):
             pivot_table=pd.DataFrame(pivot)
             df=pd.DataFrame(dataframe)
             category = pivot_table.index[row]
+            month=col
             #month = pd.Period(col, freq='M')  # Convert string back to Period
             # Filter the dataframe based on the selected category and month
             filtered_df = df[(df['Kategorie'] == category) & (df['Month'] == month)]
