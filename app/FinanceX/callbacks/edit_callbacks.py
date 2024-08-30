f98019530c92733111b5769d5c3e6c2c02ede6c4
alkemata <42153661+alkemata@users.noreply.github.com>
2024-08-30 13:48:35 +0200

    correct

--- app/FinanceX/callbacks/edit_callbacks.py
+++ app/FinanceX/callbacks/edit_callbacks.py
@@ -99,7 +99,7 @@ def update_file_account(n_clicks, file1, file2, file3, file4):
             return log_message, no_update, no_update
         log_message += res['msg']+'\n'
         account_data=df
-        log_message += 'Accounts configuration file loaded.'
+        log_message += 'Accounts configuration file loaded - '+str(categories.columns)
         categories=functions.pivot_table(file4,df)
         category_order=functions.load_categories(file4)
         #layout2=layout_categories(categories,account_data,category_order)
