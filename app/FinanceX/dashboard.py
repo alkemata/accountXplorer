2ac70e15b101158a6ccb0409e6c7812af1a82ef4
alkemata <42153661+alkemata@users.noreply.github.com>
2024-09-01 18:15:32 +0200

    correct

--- app/FinanceX/dashboard.py
+++ app/FinanceX/dashboard.py
@@ -126,10 +126,9 @@ def create_dash_app(flask_server):
             ]
         ) 
 
-#    occurences=functions.load_occurences('occurrences.csv') 
- #   end_of_month = last_update.replace(day=28) + pd.offsets.MonthEnd(1)
- #   filtered_occ = occurences[(occurences['date'] >= last_update) & (occurences['date'] <= end_of_month)]
-    filtered_occ=pd.DataFrame()
+    occurences=functions.load_occurences('occurrences.csv') 
+    end_of_month = last_update.replace(day=28) + pd.offsets.MonthEnd(1)
+    filtered_occ = occurences[(occurences['date'] >= last_update) & (occurences['date'] <= end_of_month)]
 
     plan_layout=html.Div([
         html.H2('Filtered DataTable'),
@@ -196,7 +195,7 @@ def create_dash_app(flask_server):
     def layout_main():
         layout=html.Div(
         style={'display': 'flex', 'flex-direction': 'column', 'padding': '10px'},  # Makes layout responsive
-        children=[param_layout,current_spend_layout])
+        children=[param_layout,current_spend_layout,plan_layout])
         return layout
 
     appdash.layout=layout_main()
