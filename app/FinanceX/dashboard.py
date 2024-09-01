9a7207b92f66cd6ba839913c225eeac0847434a1
alkemata <42153661+alkemata@users.noreply.github.com>
2024-09-01 17:41:51 +0200

    correct

--- app/FinanceX/dashboard.py
+++ app/FinanceX/dashboard.py
@@ -140,7 +140,7 @@ def create_dash_app(flask_server):
         mask = (df['Buchungsdatum'] >= first_day_last_month) & (df['Buchungsdatum'] <= last_day_last_month)
         last_month_df = df.loc[mask]
         selected_data = last_month_df[last_month_df['Buchungsdatum'].dt.day.astype(int) == selected_date]
-        selected_data=selected_data[["Buchungsdatum", "Empfaenger","Verwendungszweck","Betrag","Kategorie"]
+        selected_data=selected_data[["Buchungsdatum", "Empfaenger","Verwendungszweck","Betrag","Kategorie"]]
  
         return selected_data.to_dict('records')
 
