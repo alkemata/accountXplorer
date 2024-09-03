9ccc6b281248e459c384f0580ceef4db34f3ebbe
alkemata <42153661+alkemata@users.noreply.github.com>
2024-09-03 11:25:44 +0200

    correct

--- app/FinanceX/functions.py
+++ app/FinanceX/functions.py
@@ -64,7 +64,7 @@ def detect_transfers(row):
 
 def load_data(file):
         df = pd.read_csv(os.path.join(ressources_dir,file),sep=',')
-        df['Buchungsdatum'] = pd.to_datetime(df['Buchungsdatum'], format='%d.%m.%Y')
+        df['Buchungsdatum'] = pd.to_datetime(df['Buchungsdatum'])#, format='%d.%m.%Y')
         #df['Betrag'] = pd.to_numeric(df['Betrag'].replace(',','.',regex=True), errors='coerce')
         return df
 
