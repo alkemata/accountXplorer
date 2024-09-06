3c23678382dea73fbe3aa7db7a80905a8abcfe7c
alkemata <42153661+alkemata@users.noreply.github.com>
2024-09-06 15:20:14 +0200

    add fiter table

--- app/FinanceX/edit.py
+++ app/FinanceX/edit.py
@@ -48,6 +48,8 @@ def create_dash_app(flask_server):
                 columnDefs=column_defs,
                 rowData=df.to_dict('records'),
                 defaultColDef={"resizable": True},  # Make all columns resizable
+                dashGridOptions={
+                 'defaultColDef': {'filter': True, 'sortable': True}}
             )
         ])
 
