8104b464dd7d9c3837d39cdd794c15e375eae429
alkemata <42153661+alkemata@users.noreply.github.com>
2024-09-08 21:42:43 +0200

    debug

--- app/FinanceX/yearview.py
+++ app/FinanceX/yearview.py
@@ -154,9 +154,9 @@ def create_dash_app(flask_server):
             col = active_cell['column_id']
             category = pivot_table.iloc[row]['Category']
             month=col
-            print(month)
             #month = pd.Period(col, freq='M')  # Convert string back to Period
             # Filter the dataframe based on the selected category and month
+            print(df['Category'] == category) & (df['Month'] == int(month)))
             filtered_df = df[(df['Category'] == category) & (df['Month'] == int(month))]
             filtered_df = filtered_df.drop(columns=['Month'])
              #filtered_df['Buchungsdatum'] = filtered_df['Buchungsdatum'].astype(str)
