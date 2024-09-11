62a1bbe88b553a8b3a53e06d620d0be91e17a4b7
alkemata <42153661+alkemata@users.noreply.github.com>
2024-09-11 18:23:22 +0200

    correct

--- app/FinanceX/dashboard.py
+++ app/FinanceX/dashboard.py
@@ -66,14 +66,16 @@ def create_dash_app(flask_server):
         # Filter DataFrame for last month
         mask_fix = (df['Month'] == month) & (df['Betrag']<0) &(df['Notiz']=='-')
         month_df_fix = df.loc[mask_fix]
-        month_df_fix['Notiz']='Variable'
+
         # Group by day and sum amounts
         daily_sum_fix = month_df_fix.groupby(month_df_fix['Buchungsdatum'].dt.day)['Betrag'].sum().reset_index()
+        daily_sum_fix['Notiz']='Variable'
         mask_nonfix = (df['Month'] == month) & (df['Betrag']<0) &(df['Notiz']!='-')
         month_df_nonfix = df.loc[mask_nonfix]
-        month_df_nonfix['Notiz']='Fixe'
+        
         # Group by day and sum amounts
-        daily_sum_nonfix = month_df_nonfix.groupby(month_df_nonfix['Buchungsdatum'].dt.day)['Betrag'].sum().reset_index()    
+        daily_sum_nonfix = month_df_nonfix.groupby(month_df_nonfix['Buchungsdatum'].dt.day)['Betrag'].sum().reset_index()   
+        daily_sum_nonfix['Notiz']='Fixe'
         daily_sum= pd.concat([daily_sum_fix, daily_sum_nonfix], ignore_index=True)   
         daily_sum.rename(columns={'Buchungsdatum': 'day', 'Betrag': 'total_amount'}, inplace=True)        
         return daily_sum
